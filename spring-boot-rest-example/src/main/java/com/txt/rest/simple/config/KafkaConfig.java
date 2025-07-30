0cb5b7619f1030e82a2280af16328f915b358a12
ThuongTX <trinhxuan92@gmail.com>
2025-07-30 23:43:49 +0700

    spring-boot-rest-example: add Dockerfile and kafka config

---
+++ spring-boot-rest-example/src/main/java/com/txt/rest/simple/config/KafkaConfig.java
@@ -0,0 +1,20 @@
+package com.txt.rest.simple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory(ConsumerFactory consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+}
